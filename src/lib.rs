c96c51683bf780f1f73ec130698f2a115daa05c2
Matthew Thompson <mat@matthewjthompson.co.uk>
2024-04-18 21:40:21 +0100

    Add rustdocs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+//! # arnak
+//!
+//! A Rust library for the [Board Game Geek XML API](https://boardgamegeek.com/wiki/page/BGG_XML_API2).
+//!
+//! ## Example:
+//! ```rust
+//! let api = BoardGameGeekApi::new();
+//! let collection = api.collection().get_owned("blueearbgg").await?;
+//!
+//! for game in collection.games {
+//!     match game.game_type {
+//!         GameType::BoardGame => println!("{}", game.name),
+//!         GameType::BoardGameExpansion => println!("{} [expansion]", game.name),
+//!     }
+//! }
+//! ```
+//!
+
 mod api;
 pub use api::*;
 
