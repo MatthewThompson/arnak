126eef210388c342f5b230fdbb65b94186b67eae
Matthew Thompson <mat@matthewjthompson.co.uk>
2024-04-18 23:07:37 +0100

    Improve error type

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,4 @@
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use std::error::Error as StdError;
 use std::fmt;
 
@@ -11,10 +11,19 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// is typically returned in some case an error shouldn't happen.
 #[derive(Debug)]
 pub enum Error {
+    /// An error was returned making the HTTP request, or an error
+    /// status code was returned.
     HttpError(reqwest::Error),
-    ParseError(serde_xml_rs::Error),
-    ApiError(String),
-    LocalError(String),
+    /// An error occured attempting to parse the response from
+    /// the API into the expected type.
+    UnexpectedResponseError(serde_xml_rs::Error),
+    /// The request tried too many times and timed out before the
+    /// data was ready to be returned by the API.
+    MaxRetryError(u32),
+    /// The username requested was not found.
+    UnknownUsernameError,
+    /// The API returned a list of errors that we do not recognise.
+    UnknownApiErrors(Vec<String>),
 }
 
 impl From<reqwest::Error> for Error {
@@ -25,7 +34,7 @@ impl From<reqwest::Error> for Error {
 
 impl From<serde_xml_rs::Error> for Error {
     fn from(err: serde_xml_rs::Error) -> Self {
-        Error::ParseError(err)
+        Error::UnexpectedResponseError(err)
     }
 }
 
@@ -33,9 +42,16 @@ impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Error::HttpError(e) => write!(f, "error making request: {}", e),
-            Error::ParseError(e) => write!(f, "error parsing output: {}", e),
-            Error::ApiError(s) => write!(f, "{s}"),
-            Error::LocalError(s) => write!(f, "{s}"),
+            Error::UnexpectedResponseError(e) => write!(f, "error parsing output: {}", e),
+            Error::MaxRetryError(retries) => {
+                write!(f, "data still not ready after {retries} retries, aborting")
+            }
+            Error::UnknownUsernameError => write!(f, "username not found"),
+            Error::UnknownApiErrors(messages) => match messages.len() {
+                0 => write!(f, "got error from API with no message"),
+                1 => write!(f, "got unknown error from API: {}", messages[0]),
+                _ => write!(f, "got errors from API: {}", messages.join(", ")),
+            },
         }
     }
 }
@@ -44,20 +60,34 @@ impl StdError for Error {
     fn source(&self) -> Option<&(dyn StdError + 'static)> {
         match &self {
             Error::HttpError(e) => Some(e),
-            Error::ParseError(e) => Some(e),
-            Error::ApiError(_) => None,
-            Error::LocalError(_) => None,
+            Error::UnexpectedResponseError(e) => Some(e),
+            Error::MaxRetryError(_) => None,
+            Error::UnknownUsernameError => None,
+            Error::UnknownApiErrors(_) => None,
         }
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
 pub(crate) struct ApiXmlErrors {
     #[serde(rename = "$value")]
     pub(crate) errors: Vec<ApiXmlError>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
 pub(crate) struct ApiXmlError {
     pub(crate) message: String,
+}
+
+impl From<ApiXmlErrors> for Error {
+    fn from(errors: ApiXmlErrors) -> Self {
+        let error_messages: Vec<String> = errors.errors.into_iter().map(|e| e.message).collect();
+        if error_messages.len() == 1 {
+            let error_message = &error_messages[0];
+            if error_message == "Invalid username specified" {
+                return Self::UnknownUsernameError;
+            }
+        }
+        Self::UnknownApiErrors(error_messages)
+    }
 }
