0666ca69e247cf04a0b2fef1604372883e301880
Matthew Thompson <mat@matthewjthompson.co.uk>
2024-08-25 16:30:51 +0100

    Add IntoQueryParam trait

--- src/query_param.rs
+++ src/query_param.rs
@@ -1,1 +1,7 @@
 pub(crate) type QueryParam<'a> = (&'a str, String);
+
+// Can't use the from and into traits because it needs to be defined for
+// foreign types. But since it's not a public trait it doesn't really matter.
+pub(crate) trait IntoQueryParam {
+    fn into_query_param(self, key: &str) -> QueryParam<'_>;
+}
